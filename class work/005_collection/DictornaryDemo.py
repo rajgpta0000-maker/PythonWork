0db5fde4b6b28b3497bcdaae24f9857961ff6d16
RajAssignment <rajgpta0000@gmail.com>
2026-05-05 11:00:03 +0530

    Hello

---
+++ class work/005_collection/DictornaryDemo.py
@@ -0,0 +1,75 @@
+# person = {
+#     "Name" : "Raj",
+#     "Age" : 25,
+#     "City" : "Delhi",
+#     "lang" : ["Hindi", "English"],
+#     123 : "abc",
+#     True : "boolean",
+#     (10,20,30) : "tuple"
+#     [10,20,30] : "list"
+# }
+# print(person)
+
+# other way to create dictionary
+# d=dict(name="Raj", age=25, city="Delhi")
+# print(d)
+
+cn = {
+    "india" : "IN",
+    "usa" : "US",
+    "australia" : "AUS",
+
+}
+# print(cn)
+# print(cn.get("india")) # returns None if key is not found
+# # print(cn['usa1']) # raises KeyError if key is not found
+# print("Hello") # will be printed even if key is not found
+
+# print(cn.keys()) # shows all keys in the dictionary
+# print(cn.values()) # shows all values in the dictionary
+# print(cn.items()) # shows all key-value pairs in the dictionary
+
+# for i,j in cn.items(): # i ma key ko store karega aur j value ko store karega
+#     print(i,j)
+
+# cn["india"] = "INR" # update hota hai
+# cn.update({"abc":"xyz","india" : "INR"}) # update or add hota hai
+# print(cn)
+
+# cn.pop("india")  # key ko remove karta hai
+# cn.popitem() # last item ko remove karta hai
+# cn.clear() # sabhi items ko remove karta hai
+# del cn # dictionary ko delete kar deta hai
+# print(cn)
+
+# k = cn.copy() # dictionary ka copy banata hai
+# k= cn # k aur cn dono same dictionary ko point kar rahe hai
+# k.update({"abc":"xyz"}) # update karta hai to dono dictionary update ho jati hai
+# print(k)
+# print(cn)
+
+
+
+# for i,j in students["marks"].items(): # nested dictionary me loop lagane ke liye
+#     print(i,j)
+
+#     x = ('key1', 'key2', 'key3')
+#     y = 0
+#     print(dict.fromkeys(x,y)) # fromkeys method se ek naya dictionary banata hai jisme keys x ke elements honge aur unki value y hogi
+
+#     thisdict = dict.fromkeys(x,y)
+#     print(thisdict)
+# # print(students) # all items print karega
+# # print(students["name"]) # specific key ka value print karega
+# # print(students["marks"]["python"]) # nested dictionary me specific key ka value print karega
+
+x = ("key1", "key2", "key3")
+y = (10,20,30)
+
+d = zip(x,y) # zip function se do list ko ek saath zip kar sakte hai
+print(list(d)) # zip object ko list me convert kar ke print karega
+print(tuple(d)) # zip object ko tuple me convert kar ke print karega
+print(set(d)) # zip object ko set me convert kar ke print karega
+
+k = {"a":1, "b":2, "c":3}
+
---
+++ class work/005_collection/SetDemo.py
@@ -0,0 +1,38 @@
+# s = {10,20,30,40,50}
+# print(s)
+# s.add(60)
+# print(s)
+# print(len(s))
+# s.remove(30)
+# print(s)
+# print(20 in s)
+# update = {70,80,90}
+# s.update(update)
+# print(s)
+# a = {10,20,30,40,50,True,0}
+# b = {30,40,50,60,70,False,1}
+# print(a.update(b))
+
+# print(a)
+# # k = a.union(b)
+# k = a|b
+# # print(k)
+
+# a.intersection_update(b)
+# k = a.intersection(b)
+# k = a & b
+# print(a)
+
+# a.difference_update(b)
+# k = a.difference(b)
+# k = a - b
+# print(k)
+
+# a.symmetric_difference_update(b)
+# print(a)
+# k = a.symmetric_difference(b)
+# print(k)
+# k = a ^ b
+# print(k)
+
+a = {10,20,30}
